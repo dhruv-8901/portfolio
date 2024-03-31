27526d5f3e944482e85295127476fe760f581184
DESKTOP-CKJJILT\admin <pateldhruv123890@gmail.com>
2024-03-31 14:31:38 +0530

    initial commit

---
+++ src/portfolio.js
@@ -0,0 +1,64 @@
+const header = {
+  homepage: "https://dhruv-8901.github.io/portfolio",
+  title: "DP.",
+};
+
+const about = {
+  name: "Dhruv Patel",
+  role: "MERN stack developer",
+  description:
+    "Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.",
+  resume: "https://example.com",
+  social: {
+    linkedin: "https://linkedin.com",
+    github: "https://github.com",
+  },
+};
+
+const projects = [
+  {
+    name: "Project 1",
+    description:
+      "Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam",
+    stack: ["SASS", "TypeScript", "React"],
+    sourceCode: "https://github.com",
+    livePreview: "https://github.com",
+  },
+  {
+    name: "Project 2",
+    description:
+      "Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam",
+    stack: ["SASS", "TypeScript", "React"],
+    sourceCode: "https://github.com",
+    livePreview: "https://github.com",
+  },
+  {
+    name: "Project 3",
+    description:
+      "Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam",
+    stack: ["SASS", "TypeScript", "React"],
+    sourceCode: "https://github.com",
+    livePreview: "https://github.com",
+  },
+];
+
+const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Redux",
+  "SASS",
+  "Material UI",
+  "Git",
+  "CI/CD",
+  "Jest",
+  "Enzyme",
+];
+
+const contact = {
+  email: "pateldhruv123890@gmail.com",
+};
+
+export { header, about, projects, skills, contact };
