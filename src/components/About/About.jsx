d1b4e4835c1f8fe3d3ab037942005677451dc8ff
dhruv-8901 <pateldhruv123890@gmail.com>
2024-08-05 22:21:51 +0530

    add skills

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import { about } from "../../portfolio";
-import myPdf from "../../data/Dhruv_resume.pdf";
+import myPdf from "../../data/Dhruv-resume.pdf";
 import "./About.css";
 
 const About = () => {
--- src/portfolio.js
+++ src/portfolio.js
@@ -5,9 +5,9 @@ const header = {
 
 const about = {
   name: "Dhruv Patel",
-  role: "MERN stack developer",
+  role: "Node.js developer",
   description:
-    "Experienced MERN stack developer with over 3+ years of experience proficient in crafting scalable web applications. Skilled in Node.js, Express.js, React.js, and MongoDB. Proficient in RESTful APIs, database management, AWS EC2 deployment, and hosting. Strong emphasis on quality performance and reliability.",
+    "Experienced Node.js developer with over 3.5+ years of experience proficient in crafting scalable web applications. Skilled in Node.js, Express.js, React.js, and MongoDB. Proficient in RESTful APIs, database management, AWS EC2 deployment, and hosting. Strong emphasis on quality performance and reliability.",
   resume: "https://example.com",
   social: {
     linkedin: "https://www.linkedin.com/in/dhruv-patel-2a15a31b9/",
